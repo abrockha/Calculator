62112a2a334d8f1e55e505643bac298dce56449c
adambrockhaus <adam.m.brockhaus@gmail.com>
2021-06-30 07:43:16 -0500

    changed methods to static

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,31 +1,25 @@
 
 public class Calculator {
 	
-	private int firstNum;
-	private int secondNum;
 	
-	public Calculator(int num1, int num2) {
-		this.firstNum = num1;
-		this.secondNum = num2;
-	}
 	
-	public int add(int firstNum,int secondNum) {
+	public static int add(int firstNum,int secondNum) {
 		return firstNum + secondNum;
 	}
 	
-	public int subtraction(int firstNum,int secondNum) {
+	public static int subtraction(int firstNum,int secondNum) {
 		return firstNum - secondNum;
 	}
 	
-	public int multiplication(int firstNum,int secondNum) {
+	public static int multiplication(int firstNum,int secondNum) {
 		return firstNum * secondNum;
 	}
 	
-	public int division(int firstNum,int secondNum) {
+	public static int division(int firstNum,int secondNum) {
 		return firstNum / secondNum;
 	}
 	
-	public int square(int firstNum,int secondNum) {
+	public static int square(int firstNum,int secondNum) {
 		return firstNum * firstNum;
 	}
 }
